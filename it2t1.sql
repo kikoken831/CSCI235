a6ae833d5f5d83b2d61ed5bf0032eecdc699c783
Oracle <oracle@localhost.localdomain>
2021-11-07 20:00:28 +0800

    Pushing IT2T1

---
+++ it2t1.sql
@@ -0,0 +1,39 @@
+SPOOL IT2Task1.txt
+SET ECHO ON 
+SET FEEDBACK ON 
+SET LINESIZE 100 
+SET PAGESIZE 200 
+SET SERVEROUTPUT ON
+/*
+select r_regionkey, r_name, n_nationkey, n_name
+from region, nation
+where r_regionkey = n_regionkey
+order by r_regionkey,n_nationkey;
+*/
+declare
+--declare block
+prevRegion REGIONTest.r_regionkey%type := -1;
+
+begin
+--execution block
+for QRow in (select r_regionkey, r_name, n_nationkey, n_name
+from region, nation
+where r_regionkey = n_regionkey
+order by r_regionkey,n_nationkey)
+
+loop
+if (Qrow.r_regionkey != prevRegion) then 
+	dbms_output.put_line(QRow.r_regionkey || ' ' || QRow.r_name || ': ');
+	for IRow in (select n_name from region, nation where r_regionkey = n_regionkey)
+	loop
+	dbms_output.put_line(IRow
+	end loop;
+	prevRegion := Qrow.r_regionkey;
+end if;
+end loop;
+
+--exception block
+end;
+/
+SPOOL OFF;
+
